c44d8ac0ffc9e46267e342d40c38ffb32c761599
Nikita <n_timchuk@bk.ru>
2025-05-07 00:13:39 +0300

    exportToExcel change insert

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -366,31 +366,22 @@ class ExportToExcel extends Main {
 
         String printAll = "SELECT * FROM " + table;
         try (PreparedStatement pst = con.prepareStatement(printAll); ResultSet rs = pst.executeQuery()) {
+            ResultSetMetaData rsmd = rs.getMetaData();
+            int length = rsmd.getColumnCount();
+
             Workbook wb = new XSSFWorkbook();
             Sheet sheet = wb.createSheet("task 1");
             Row row = sheet.createRow(0);
-            row.createCell(0).setCellValue(rs.getMetaData().getColumnName(1));
-            row.createCell(1).setCellValue(rs.getMetaData().getColumnName(2));
-            row.createCell(2).setCellValue(rs.getMetaData().getColumnName(3));
-            row.createCell(3).setCellValue(rs.getMetaData().getColumnName(4));
-            row.createCell(4).setCellValue(rs.getMetaData().getColumnName(5));
-            row.createCell(5).setCellValue(rs.getMetaData().getColumnName(6));
-            row.createCell(6).setCellValue(rs.getMetaData().getColumnName(7));
-            row.createCell(7).setCellValue(rs.getMetaData().getColumnName(8));
-            row.createCell(8).setCellValue(rs.getMetaData().getColumnName(9));
+            for (int i = 0; i < length; i++) {
+                row.createCell(i).setCellValue(rsmd.getColumnName(i + 1));
+            }
 
             int rowIndex = 1;
             while (rs.next()) {
                 Row row1 = sheet.createRow(rowIndex++);
-                row1.createCell(0).setCellValue(rs.getInt(1));
-                row1.createCell(1).setCellValue(rs.getInt(2));
-                row1.createCell(2).setCellValue(rs.getInt(3));
-                row1.createCell(3).setCellValue(rs.getInt(4));
-                row1.createCell(4).setCellValue(rs.getInt(5));
-                row1.createCell(5).setCellValue(rs.getInt(6));
-                row1.createCell(6).setCellValue(rs.getInt(7));
-                row1.createCell(7).setCellValue(rs.getInt(8));
-                row1.createCell(8).setCellValue(rs.getInt(9));
+                for (int i = 0; i < length; i++) {
+                    row1.createCell(i).setCellValue(rs.getInt(i + 1));
+                }
 
             }
             int columnCount = sheet.getRow(0).getPhysicalNumberOfCells();
